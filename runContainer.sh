56793f6e3213446ddf82cccf7c781f2f973f30cf
Kah Siong Tan <jax79sg@yahoo.com.sg>
2019-07-17 08:14:14 +0800

    First commit

---
+++ runContainer.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker run -it --runtime=nvidia bert_base /bin/bash
---
+++ test_glue.sh
@@ -0,0 +1,16 @@
+export BERT_BASE_DIR=/root/pretrained/wwm_uncased_L-24_H-1024_A-16
+export GLUE_DIR=/root/glue_data
+
+python3.5 run_classifier.py \
+  --task_name=MRPC \
+  --do_train=true \
+  --do_eval=true \
+  --data_dir=$GLUE_DIR/MRPC \
+  --vocab_file=$BERT_BASE_DIR/vocab.txt \
+  --bert_config_file=$BERT_BASE_DIR/bert_config.json \
+  --init_checkpoint=$BERT_BASE_DIR/bert_model.ckpt \
+  --max_seq_length=128 \
+  --train_batch_size=32 \
+  --learning_rate=2e-5 \
+  --num_train_epochs=3.0 \
+  --output_dir=/tmp/mrpc_output/
