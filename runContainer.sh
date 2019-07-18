bead71aae60d1f176002df5f5a36029efa1821d0
jax79sg <jax79sg@yahoo.com.sg>
2019-07-18 10:01:32 +0800

    added bind mount

--- runContainer.sh
+++ runContainer.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-docker run -it --runtime=nvidia bert_base /bin/bash
+# Note: change the path after -v to an existing folder on your host. 
+docker run -it --runtime=nvidia -v /mnt/DATA/projects/docker_bert/persistentfolder:/root/bert/persistentfolder  bert_base /bin/bash
